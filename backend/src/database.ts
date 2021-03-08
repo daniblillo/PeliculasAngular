cbfc78716c653c40741aba80098c33702b119037
daniblillo <daniel.10.betico@gmail.com>
2021-03-08 22:45:34 +0100

    MongoAtlas

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose'
 
 export async function startConnection() {
-    const db = await connect('mongodb://localhost/db',{
+    const db = await connect('mongodb+srv://mongo:Hola1234@cluster0.eloh1.mongodb.net/db?',{
         useNewUrlParser: true,
         useFindAndModify: false 
     });
